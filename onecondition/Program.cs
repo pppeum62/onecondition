433c2118d77a97f7bdf29722261a736ce00381aa
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-06 18:59:47 +0900

    문제 풀이 - 중첩 조건문을 논리 연산자로 수정

--- onecondition/Program.cs
+++ onecondition/Program.cs
@@ -10,14 +10,12 @@ namespace onecondition
     {
         static void Main(string[] args)
         {
+            Console.Write("x값을 입력하세요 >> ");
             int x = int.Parse(Console.ReadLine());
 
-            if (x > 10)
+            if (x > 10 && x < 20)
             {
-                if(x < 20)
-                {
-                    Console.WriteLine("조건에 맞습니다.");
-                }
+                Console.WriteLine("조건에 맞습니다.");
             }
         }
     }
